fdc5f47fef0b94bed47fe9842ec2bdc687ab45f7
Tint Naing Win <me@tintnaingwin.com>
2019-09-24 00:51:51 +0630

    wip

--- config/kuu-pyaung.php
+++ config/kuu-pyaung.php
@@ -1,8 +1,38 @@
 <?php
 
 return [
+
+    /*
+     * These directories only will be convert.
+     */
     'include_files' => [
         'views',
         'lang', // lang/my
+    ],
+
+    'database' => [
+       /*
+        * These tables will be excluded from the convert.
+        */
+        'exclude_tables' => [
+            'migrations',
+            'telescope_entries',
+            'telescope_entries_tags',
+            'telescope_monitoring',
+        ],
+
+        /*
+         * These data types will be include from the convert.
+         *
+         * Recommend data type is string.
+         */
+        'include_data_types' => [
+            'char',
+            'varchar',
+            'tinytext',
+            'text',
+            'mediumtext',
+            'longtext',
+        ]
     ]
 ];
